7823f74d9de1f51ca69b0dbb8397592cf4357abd
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-29 14:37:46 -0500

    get-random-meme-url onClick

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -8,11 +8,18 @@ import memedata from "../memesData.js"
     Log the url of the image to the 
 */ 
 
+let url
+
 export default function Meme(){
     function getRandomImage(){
-        const id = Math.floor(Math.random()*100)
-        const url = memedata.data.memes[id].url
+        // const id = Math.floor(Math.random()*100)
+        // const url = memedata.data.memes[id].url
+        // console.log(url)
+        const memeArray = memedata.data.memes
+        const randomNumber = Math.floor(Math.random() * memeArray.length)
+        url = memeArray[randomNumber].url
         console.log(url)
+
     }
     return(
         <main>
@@ -28,6 +35,7 @@ export default function Meme(){
                 className="form--input"
                 />
                 <button onClick={getRandomImage} className="form--button ">Get a new meme image</button>
+                <h4>{url}</h4>
             </div>
         </main>
     )
