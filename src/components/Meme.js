92eda78e78cc760cee519609ec8e51294105da6c
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-29 14:31:27 -0500

    random-url-challenge - Harika

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,22 @@
 import React from "react";
+import memedata from "../memesData.js"
+
+/*
+    Challenge : Get a random image from the 'memesData array
+    When the "new meme image" button is clicked.
+
+    Log the url of the image to the 
+*/ 
 
 export default function Meme(){
+    function getRandomImage(){
+        const id = Math.floor(Math.random()*100)
+        const url = memedata.data.memes[id].url
+        console.log(url)
+    }
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                 type="text"
                 placeholder="top-text"
@@ -14,8 +27,8 @@ export default function Meme(){
                 placeholder="bottom-text"
                 className="form--input"
                 />
-                <button className="form--button ">Get a new meme image</button>
-            </form>
+                <button onClick={getRandomImage} className="form--button ">Get a new meme image</button>
+            </div>
         </main>
     )
 }
