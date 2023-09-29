de3949ee2d97b61c6fea49702dddfa7d1d60d889
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-29 16:36:23 -0500

    Challenge 1

--- src/ternary-practice/Main.js
+++ src/ternary-practice/Main.js
@@ -1,20 +1,18 @@
 import React from "react";
 
-export default function Main(){
-    const isGoingOut = false
-    
-    // use ternary here
-    const answer = isGoingOut ? "Yes" : "No"
+/*
+    Challenge 1 : move our ternary directly inside of our JSX
+    so that "Yes" and "No" are determined inside the <h1>
 
-    // if(isGoingOut === true){
-    //     answer = "Yes"
-    // }else{
-    //     answer = "No"
-    // }
+*/ 
+
+export default function Main(){
+    const isGoingOut = true
+    //const answer = isGoingOut ? "Yes" : "No" // use ternary here
     return(
         <div className="state">
             <h1>Do I feel like going out tonight?</h1>
-            <h1>{answer}</h1>
+            <h1>{isGoingOut ? "Yes" : "No"}</h1>
         </div>
     )
 }
