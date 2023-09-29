878b486b1c0cfe7e48de0b936914fb6d4126cc91
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-29 15:49:31 -0500

    Challenge 3

--- src/connundrumResolved/Counter.js
+++ src/connundrumResolved/Counter.js
@@ -2,15 +2,21 @@ import React, { useState } from "react";
 
 /*
     Challenge 1 : Set up state to tract our count (initial value is 0)
+    Challenge 2 : whenever the + button is clicked just console.log("add")
+    Challenge 3 : add 1 to the count every time + button is clicked
+    Challenge 4
 */ 
 
 export default function Counter(){ 
     const [count,setCount] =  useState(0)
+    function add(){
+        setCount(count + 1)
+    }
     return(
         <div className="counter">
             <button>-</button>
             <h3>{count}</h3>
-            <button>+</button>
+            <button onClick={add}>+</button>
         </div>
     )
 }
