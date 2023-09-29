4aa3425ce6454fb2bec9e3378b77cf1c0ee0ef8a
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-29 14:20:18 -0500

    memeData

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-//import Header from "./components/Header";
-//import Meme from "./components/Meme";
-import Events from "./event-listener/Events";
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+//import Events from "./event-listener/Events";
 
 export default function App(){
     return(
         <div>
-            {/* <Header/>
-            <Meme/> */}
-            <Events/>
+            <Header/>
+            <Meme/>
+            {/* <Events/> */}
         </div>
     )
 }
