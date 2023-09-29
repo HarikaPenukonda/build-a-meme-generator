137e9330da2889d128ed38500cfff06fdb434c96
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-29 15:39:24 -0500

    Challenge 2

--- src/connundrumResolved/State.js
+++ src/connundrumResolved/State.js
@@ -4,14 +4,23 @@ import React from "react";
     Challenge : Replace our hard-coded "Yes" on the page with some state initiated with React.useState( )
 */ 
 
+/*
+    Challenge 2 : Create a function called `handleClick that runs setIsImportant("No")
+        - add a click event listener to the div.state-value that runs `handleClick` when the div is clicked
+*/
+
 export default function State(){
-    const [isImportant,setResult] = React.useState("No")
-    console.log(result)
+    const [isImportant,setIsImportant] = React.useState("Yes")
+
+    function handleClick(){
+        setIsImportant("No")
+    }
+    
     return(
         <div className="state">
             <h1 className="state--title">Is state important to know</h1>
             <div className="state-value">
-                <h1>{isImportant}</h1>
+                <h1 onClick={handleClick}>{isImportant}</h1>
             </div>
         </div>
     )
