aabe3410d957c04d41a4f8398660d854ec1e9219
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-29 14:17:51 -0500

    onMouseOver and onClick events

--- src/event-listener/Events.js
+++ src/event-listener/Events.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+/*
+    Challenge :
+        Log something to the console when the mouse hovers over the image 
+*/
+
 export default function Events(){
     function handleClick (){
         console.log("I am clicked")
+    }
+
+    function moveOver(){
+        console.log("someone scrolled on me")
     }
     return(
         <div className="container">
@@ -10,6 +19,7 @@ export default function Events(){
                 src="./images/troll.png"
                 alt=""
                 height="150"
+                onMouseOver={moveOver}
             />
             <button onClick={handleClick}>click me</button>
 
