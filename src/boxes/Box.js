d27d6b2bef1aec69f3ee7543a89c82c7679bbc9a
HarikaPenukonda <penukondaharika@gmail.com>
2023-10-03 10:49:00 -0500

    Boxes challenge part 2

--- src/boxes/Box.js
+++ src/boxes/Box.js
@@ -1,37 +1,16 @@
-import React, { useState } from "react";
-import boxesData from "./boxes";
+import React from "react";
+import boxesData from "./boxes-data";
 
-/*
-    Challenge part 1 :
-        1. Initialize state with default value of the array pulled in from boxes.js
-        2. Map over the state array and display each one as an empty square(black border, transparent bg)
-*/ 
-/*
-    Challenge part 2 : use a ternary determine the background color
-    If darkMode is true, set it to #222222 if false set it to #cccccc
-
-*/ 
 export default function Box(props){
-
-    const[boxes,setBoxes] = useState(boxesData)
-
     const styles = {
-        backgroundColor : props.darkMode ? "#222222" : "#cccccc" 
+        backgroundColor : props.on ? "#222222" : "none" 
     }
-
-    const boxElements = boxes.map(data => (
+    // console.log(props.on)
+    return(
         <div 
             className="box"
-            key={data.id}
             style={styles}>
-
+        
         </div>
-    ))
-
-    return(
-        <main>
-            <h1>Boxes will go here</h1>
-            {boxElements}
-        </main>
     )
 }
--- src/boxes/boxes.js
+++ src/boxes/boxes.js
@@ -1,26 +1,22 @@
-export default[
-    {
-        id:1,
-        on: true
-    },
-    {
-        id:2,
-        on: false
-    },
-    {
-        id:3,
-        on: true
-    },
-    {
-        id:4,
-        on: true
-    },
-    {
-        id:5,
-        on: false
-    },
-    {
-        id:6,
-        on: false
-    }
-]
+import React, { useState } from "react";
+import boxesData from "./boxes-data";
+import Box from "./Box"
+
+export default function Boxes(){
+
+    const[boxes,setBoxes] = useState(boxesData)
+
+    const boxElements = boxes.map(data => (
+        <Box
+            key={data.id}
+            on={data.on}
+        />
+    ))
+
+    return(
+        <main>
+            <h1>Boxes will go here</h1>
+            {boxElements}
+        </main>
+    )
+}
