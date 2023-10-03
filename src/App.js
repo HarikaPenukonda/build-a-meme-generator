19673b7024e55c7c73f71b0e9777a02deb00e934
HarikaPenukonda <penukondaharika@gmail.com>
2023-10-03 14:33:09 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 
 // import Header from "./components/Header";
 // import Meme from "./components/Meme";
-import AppTwo from "./conditional-rendering-practice/AppTwo"
 
 export default function App(){
     // const[user,setUser] = useState("Joe") 
@@ -14,8 +13,6 @@ export default function App(){
             
             {/* <Header user={user}/>
             <Body user={user}/> */}
-        
-           <AppTwo/>
             
         </div>
     )
