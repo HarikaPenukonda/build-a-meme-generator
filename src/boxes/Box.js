85becd46c31530580922053591b4890834c0f6fc
HarikaPenukonda <penukondaharika@gmail.com>
2023-10-03 10:32:50 -0500

    Dynamic styles - challenge

--- src/boxes/Box.js
+++ src/boxes/Box.js
@@ -6,15 +6,24 @@ import boxesData from "./boxes";
         1. Initialize state with default value of the array pulled in from boxes.js
         2. Map over the state array and display each one as an empty square(black border, transparent bg)
 */ 
+/*
+    Challenge part 2 : use a ternary determine the background color
+    If darkMode is true, set it to #222222 if false set it to #cccccc
 
-export default function App(){
+*/ 
+export default function Box(props){
 
     const[boxes,setBoxes] = useState(boxesData)
+
+    const styles = {
+        backgroundColor : props.darkMode ? "#222222" : "#cccccc" 
+    }
 
     const boxElements = boxes.map(data => (
         <div 
             className="box"
-            key={data.id}>
+            key={data.id}
+            style={styles}>
 
         </div>
     ))
