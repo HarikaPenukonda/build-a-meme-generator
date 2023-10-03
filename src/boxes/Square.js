408d7ca7dfa5902fa125aab78d8560590b3ab012
HarikaPenukonda <penukondaharika@gmail.com>
2023-10-03 12:05:46 -0500

    Boxes Challenge Part 4, toggle function imperetive

--- src/boxes/Square.js
+++ src/boxes/Square.js
@@ -7,7 +7,22 @@ export default function Square(){
     const [squares,setSquares] = useState(boxdata)
 
     function toggle(id){
-        
+        setSquares(prevState => {
+            const newSquares = []
+            for(let i = 0; i<prevState.length;i++){
+                const currentSquare = prevState[i]
+                if(currentSquare.id === id){
+                    const updatedSquare = {
+                        ...currentSquare,
+                        on : !currentSquare.on
+                    }
+                    newSquares.push(updatedSquare)
+                }else{
+                    newSquares.push(currentSquare)
+                }
+            }
+            return newSquares
+        })
     }
 
     const squareElements = squares.map(data =>(
